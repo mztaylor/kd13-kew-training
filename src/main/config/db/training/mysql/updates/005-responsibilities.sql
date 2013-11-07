73c17bbbcebe93740f873f17405fb8c4717b9c2f
Jonathan Keller <keller.jonathan@gmail.com>
2013-11-07 09:56:23 -0800

    Adding data for campus-based workflow.

---
+++ src/main/config/db/training/mysql/updates/005-responsibilities.sql
@@ -0,0 +1,40 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+
+-- Builds responsibilities related to training application
+
+INSERT INTO KRIM_RSP_T(RSP_ID, OBJ_ID, RSP_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND)
+    VALUES('KRTRN-2', UUID(), '1', 'KR-IDM', 'Campus Application Review', '', 'Y');
+
+INSERT INTO KRIM_RSP_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, RSP_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+    VALUES('KRTRN-2-DOC', UUID(), 'KRTRN-2', '7', '13', 'AdmissionsWorkflowDocument');
+
+INSERT INTO KRIM_RSP_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, RSP_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+    VALUES('KRTRN-2-NODE', UUID(), 'KRTRN-2', '7', '16', 'DepartmentApproval');
+
+INSERT INTO KRIM_RSP_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, RSP_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+    VALUES('KRTRN-2-REQ', UUID(), 'KRTRN-2', '7', '40', 'N');
+
+INSERT INTO KRIM_RSP_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, RSP_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+    VALUES('KRTRN-2-RML', UUID(), 'KRTRN-2', '7', '41', 'N');
+
+INSERT INTO KRIM_ROLE_RSP_T(ROLE_RSP_ID, OBJ_ID, ROLE_ID, RSP_ID, ACTV_IND) 
+    VALUES('KRTRN-2-KRATRNG10002', UUID(), 'KRATRNG10002', 'KRTRN-2', 'Y');
+
+INSERT INTO KRIM_ROLE_RSP_ACTN_T(ROLE_RSP_ACTN_ID, OBJ_ID, ACTN_TYP_CD, PRIORITY_NBR, ACTN_PLCY_CD, ROLE_MBR_ID, ROLE_RSP_ID, FRC_ACTN) 
+    VALUES('KRTRN-2-KRATRNG10002', UUID(), 'A', NULL, 'F', '*', 'KRTRN-2-KRATRNG10002', 'N');
---
+++ src/main/config/db/training/mysql/updates/006-role-members.sql
@@ -0,0 +1,39 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+
+-- Builds role members related to training application
+
+INSERT INTO KRIM_ROLE_MBR_T(ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD) 
+	VALUES('KRTRN-CAMPUS-1', 1, UUID(), 'KRATRNG10002', 'dev1', 'P');
+
+INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+	VALUES('KRTRN-1-CAMPUS', UUID(), 1, 'KRTRN-CAMPUS-1', '17', '12', 'KO');
+
+
+INSERT INTO KRIM_ROLE_MBR_T(ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD) 
+	VALUES('KRTRN-CAMPUS-2', 1, UUID(), 'KRATRNG10002', 'dev2', 'P');
+
+INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+	VALUES('KRTRN-2-CAMPUS', UUID(), 1, 'KRTRN-CAMPUS-2', '17', '12', 'BL');
+
+
+INSERT INTO KRIM_ROLE_MBR_T(ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD) 
+	VALUES('KRTRN-CAMPUS-3', 1, UUID(), 'KRATRNG10002', 'idm1', 'P');
+
+INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) 
+	VALUES('KRTRN-3-CAMPUS', UUID(), 1, 'KRTRN-CAMPUS-3', '17', '12', 'BA');
